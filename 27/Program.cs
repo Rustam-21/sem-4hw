cbe549519848f5ba4b5f9b8c6dc1812c34770b62
Kalybaev Rustam <me@kalybaev.ru>
2023-05-09 21:31:21 +0500

    25 done

---
+++ 27/Program.cs
@@ -0,0 +1,27 @@
+﻿System.Console.WriteLine("Введите число ");
+
+int num = 0;
+while(!int.TryParse(System.Console.ReadLine(), out num))
+{
+    System.Console.WriteLine("Не число ");
+}
+
+
+int Function (int sum)
+{
+    string count = num.ToString();
+    int number = count.Length;
+    int number1 = 0;
+    int i = 0;
+    int summ = 0;
+    
+    while(i < number)
+    {
+        summ = summ + int.Parse(count[number1].ToString());
+        i++;
+        number1++;
+    }
+    return summ;
+}
+
+System.Console.WriteLine(Function(num));
---
+++ task29/Program.cs
@@ -0,0 +1,15 @@
+﻿﻿System.Console.WriteLine("Введите число ");
+int number = int.Parse(System.Console.ReadLine());
+string number1 = number.ToString();
+int len = number1.Length;
+
+int [] array = new int [len];
+for(int i = 0; i < len; i++)
+{
+    array[i] = int.Parse(number1[i].ToString());
+}
+
+for(int i = 0; i < len; i++ )
+{
+    System.Console.Write(array[i]);
+}
